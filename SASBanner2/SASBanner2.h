909607482eb511447adba82a0d32eef8540cd655
Anup Sukumaran <anup.sukumaran9@gmail.com>
2020-09-16 12:00:26 +0530

    Initial Commit

---
+++ SASBanner2/SASBanner2.h
@@ -0,0 +1,19 @@
+//
+//  SASBanner2.h
+//  SASBanner2
+//
+//  Created by Manu Puthoor on 16/09/20.
+//  Copyright © 2020 Manu Puthoor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SASBanner2.
+FOUNDATION_EXPORT double SASBanner2VersionNumber;
+
+//! Project version string for SASBanner2.
+FOUNDATION_EXPORT const unsigned char SASBanner2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SASBanner2/PublicHeader.h>
+
+
